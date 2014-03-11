8b569c7e02caea93d0acbe9dc68a24d24ea346d4
Sergio Gimeno <gimeno.sergio@gmail.com>
2014-03-11 17:46:23 +0100

    Add unit tests to be run by Karma.

--- test/karma.config.js
+++ test/karma.config.js
@@ -17,7 +17,8 @@ module.exports = function(config) {
     files: [
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'app/**/*.js'
+      'app/**/*.js',
+      'test/unit/*.test.js'
     ],
 
 
@@ -59,7 +60,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'PhantomJS'],
+    browsers: ['Firefox'],
 
 
     // Continuous Integration mode
